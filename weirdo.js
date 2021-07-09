1029704366e545c98db45c4fa93dd77d699cf1d4
cgeopapa <cgeocodgod@gmail.com>
2021-07-10 00:22:50 +0300

    Added GeoLocation and fixed WURFL waiting

--- weirdo.js
+++ weirdo.js
@@ -1,12 +1,24 @@
 function getBrowserInfo() {
     return {
-        "Browser": navigator.appCodeName,
-        "Version": navigator.appVersion
+        "Browser": WURFL.complete_device_name
     };
 }
 
-function getLocation() {
-    return "Will be done"
+async function getLocation() {
+    const url = "http://api.ipstack.com/check?access_key=fd08d25dc92185d49eb6a2cfc20db33b";
+    let loc;
+    await fetch(url)
+    .then(data=>{return data.json()})
+    .then(res=>{loc = res});
+    return {
+        "Continent": loc.continent_name,
+        "Country": loc.country_name,
+        "Region": loc.region_name,
+        "City": loc.city,
+        "Zip": loc.zip,
+        "Latitude": loc.latitude,
+        "Longitude": loc.longitude,
+    };
 }
 
 async function getInfo(){
@@ -16,7 +28,7 @@ async function getInfo(){
         "Browser": getBrowserInfo(),
         "OS": navigator.platform,
         "UserAgent": navigator.userAgent,
-        "GeoLocation": getLocation(),
+        "GeoLocation": await getLocation(),
         "Screen": {
             "Width": window.screen.width,
             "Height": window.screen.height,
@@ -41,8 +53,17 @@ async function dynamicallyLoadScript(url) {
             script.src = '//wurfl.io/wurfl.js';
         
             document.head.appendChild(script);
-
             await sleep(1000);
+            flag = true;
+            while(flag) {
+                flag = false;
+                try {
+                    WURFL
+                } catch(err) {
+                    flag = true;
+                    await sleep(1000)
+                }
+            }
         }
     }
 }
