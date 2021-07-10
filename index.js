eabefa7344c14ee7d5b5932c83d3222d392662ab
cgeopapa <cgeocodgod@gmail.com>
2021-07-10 21:01:28 +0300

    Added JSONViewer

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var info;
+
 async function displayInfo() {
-    document.getElementById("info").innerHTML = JSON.stringify(await getInfo(), null, 2);
+    info = await getInfo("fd08d25dc92185d49eb6a2cfc20db33b");
+
+    var jsonViewer = new JSONViewer();
+    document.getElementById("info").appendChild(jsonViewer.getContainer());
+    jsonViewer.showJSON(info, -1, -1);
+}
+
+function storeInfo() {
+    if (confirm("Are you sure? Again, by clicking here your info will be stored in a remote database and will be accessible to be viewed here in this webpage.")) {
+        if (confirm("Are you 100% sure? Anyway if you change your mind please email me here and I will delete your data asap: cgeocodgod@gmail.com")) {
+            postInfo(info, "https://webhooks.mongodb-realm.com/api/client/v2.0/app/weirdoinaction-atuuo/service/WeirdoInActionAPI/incoming_webhook/AddInfo?secret=weirdo.jsinaction")
+            .then(res => {
+                document.getElementById("confirm")
+                document.getElementById("PostMessage").innerHTML = res
+            });
+        }
+    }
 }
