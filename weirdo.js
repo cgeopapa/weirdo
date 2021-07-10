7664d2ae066f034932c1443996f73abea1e09936
cgeopapa <cgeocodgod@gmail.com>
2021-07-10 19:41:51 +0300

    Optimized weirdo.js

--- weirdo.js
+++ weirdo.js
@@ -1,41 +1,11 @@
-function getBrowserInfo() {
-    return {
-        "Browser": navigator.appName,
-        "Version": navigator.appVersion,
-        "CodeName": navigator.appCodeName
-    };
-}
-
-async function getLocation() {
-    const url = "http://api.ipstack.com/check?access_key=fd08d25dc92185d49eb6a2cfc20db33b";
-    let loc;
-    await fetch(url)
-    .then(data=>{return data.json()})
-    .then(res=>{loc = res});
-    if (loc.success != undefined) {
-        return "Please disable https only mode";
-    }
-    else {
-        return {
-            "Continent": loc.continent_name,
-            "Country": loc.country_name,
-            "Region": loc.region_name,
-            "City": loc.city,
-            "Zip": loc.zip,
-            "Latitude": loc.latitude,
-            "Longitude": loc.longitude,
-        };
-    }
-}
-
-async function getInfo(){
+async function getInfo(key){
     await dynamicallyLoadScript();
 
     const info = {
         "Browser": getBrowserInfo(),
         "OS": navigator.platform,
         "UserAgent": navigator.userAgent,
-        "GeoLocation": await getLocation(),
+        "GeoLocation": await getLocation(key),
         "Screen": {
             "Width": window.screen.width,
             "Height": window.screen.height,
@@ -50,34 +20,64 @@ async function getInfo(){
         },
     };
     return info;
-}
-
-async function dynamicallyLoadScript(url) {
-    try {
-        WURFL;
-    } catch (err) {
-        if (err instanceof ReferenceError) {
-            var script = document.createElement("script");
-            script.src = '//wurfl.io/wurfl.js';
-        
-            document.head.appendChild(script);
-            await sleep(1000);
-            flag = true;
-            while(flag) {
-                flag = false;
-                try {
-                    WURFL
-                } catch(err) {
-                    flag = true;
-                    await sleep(1000)
+    
+    function getBrowserInfo() {
+        return {
+            "Browser": navigator.appName,
+            "Version": navigator.appVersion,
+            "CodeName": navigator.appCodeName
+        };
+    }
+    
+    async function getLocation(key) {
+        const url = "http://api.ipstack.com/check?access_key="+key;
+        let loc;
+        await fetch(url)
+        .then(data=>{return data.json()})
+        .then(res=>{loc = res});
+        if (loc.success != undefined) {
+            return {"Error": loc.error};
+        }
+        else {
+            return {
+                "Continent": loc.continent_name,
+                "Country": loc.country_name,
+                "Region": loc.region_name,
+                "City": loc.city,
+                "Zip": loc.zip,
+                "Latitude": loc.latitude,
+                "Longitude": loc.longitude,
+            };
+        }
+    }
+    
+    async function dynamicallyLoadScript() {
+        try {
+            WURFL;
+        } catch (err) {
+            if (err instanceof ReferenceError) {
+                var script = document.createElement("script");
+                script.src = '//wurfl.io/wurfl.js';
+            
+                document.head.appendChild(script);
+                await sleep(1000);
+                flag = true;
+                while(flag) {
+                    flag = false;
+                    try {
+                        WURFL
+                    } catch(err) {
+                        flag = true;
+                        await sleep(1000)
+                    }
                 }
             }
         }
     }
-}
-
-async function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    })
+    
+    async function sleep(ms) {
+        return new Promise(resolve => {
+            setTimeout(resolve, ms);
+        })
+    }
 }
