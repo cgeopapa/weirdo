e4834032bfca0e84d5996e15c8376eb520ac9eef
cgeopapa <cgeocodgod@gmail.com>
2021-11-17 09:32:05 +0200

    init

---
+++ backend.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+const port = 5000;
+
+app.get('/' , (req , res)=>{
+
+   res.send('hello from simple server :)')
+
+})
+
+app.listen(port, () => {
+    console.log("Now listening...");
+})
