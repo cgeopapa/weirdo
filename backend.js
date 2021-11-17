6adc3a57906fb9e321a1ed3ac0180cc2c791e33c
cgeopapa <cgeocodgod@gmail.com>
2021-11-17 11:34:15 +0200

    working prototype

--- backend.js
+++ backend.js
@@ -1,5 +1,9 @@
+const fs = require('fs-extra')
+const bodyParser = require('body-parser');
+
 const express = require('express')
 const app = express();
+app.use(bodyParser.json())
 
 const port = 5000;
 
@@ -7,6 +11,18 @@ app.get('/' , (req , res)=>{
 
    res.send('hello from simple server :)')
 
+})
+
+app.post('/info' , (req , res)=>{
+    const info = JSON.stringify(req.body);
+    
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const date = new Date();
+    const filename = `./testing-range/bodies/${ip}-${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.json`
+    fs.outputFile(filename, info, { recursive: true }, function (err) {
+        if(err) return console.log(err);
+        res.sendStatus(200);
+    });
 })
 
 app.listen(port, () => {
